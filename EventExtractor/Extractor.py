7ed3db5ff1df1a04e6e263d8cbbe9a36b4bb2843
Haripriya Bendapudi <hari2512@gmail.com>
2016-03-26 00:11:26 +0530

    NER Tag and phrase chunking

--- EventExtractor/Extractor.py
+++ EventExtractor/Extractor.py
@@ -5,13 +5,14 @@ Created on 23-Mar-2016
 '''
 
 import json
-import nltk
-from nltk.tag.stanford import StanfordPOSTagger
 
 from article import Article
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.chunk import conlltags2tree
+from nltk.tree import Tree
 
 
 def splitToSentences(article):
@@ -29,41 +30,71 @@ def NamedEntityRecognizer(sent_tokenize_list):
 	for sent in sent_tokenize_list:
 		#tokenized_text = word_tokenize(sent)
 		classified_text = st.tag(sent.split())
-		classified_article.append(classified_text)
+		classified_article = classified_article + classified_text
 	#	for ne in classified_text:
 	#		if ne[1] != 'O':
 	#			classified_article.append(ne)		
 #	print ('\n')
 	return classified_article
 
+
+def IOBTagging(ne_tagged_set):
+	IOB_tagged = []
+	prev_tag = "O"
+	for token, tag in ne_tagged_set:
+		if tag == "O":
+			IOB_tagged.append((token,tag))
+			prev_tag = tag
+			continue
+		if tag != "O" and prev_tag == "O":
+			IOB_tagged.append((token, "B-"+tag))
+			prev_tag = tag
+		elif tag!= "O" and prev_tag == tag:
+			IOB_tagged.append((token, "I-"+tag))
+			prev_tag = tag
+		elif tag != "O" and prev_tag != tag:
+			IOB_tagged.append((token, "B-"+tag))
+			prev_tag = tag
+			
+	return IOB_tagged
+
+def getMultiWordEntityTree(ne_tagged_set):
+	IOBTagged = IOBTagging(ne_tagged_set)
+	sent_tokens, sent_ne_tags = zip(*IOBTagged)
+	sent_pos_tags = [pos for token, pos in pos_tag(sent_tokens)]
+	
+	sent_ne_pos = [(token, pos, ne) for token, pos, ne in zip(sent_tokens, sent_pos_tags, sent_ne_tags)]
+	ne_tree = conlltags2tree(sent_ne_pos)
+	return ne_tree
+			
 def PosTagger(sent_tokenize_list):
 	pos_tagged_words = []
 	#st = StanfordPOSTagger('/home/haripriya/AI/eventExtraction/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz',
 	#			'/home/haripriya/AI/eventExtraction/stanford-ner-2015-12-09/stanford-ner-3.6.0.jar',
 	#				   encoding='utf-8')
 	for sent in sent_tokenize_list:
-		pos_tagged_line = nltk.pos_tag(sent.split())
+		pos_tagged_line = pos_tag(sent.split())
 		#pos_tagged_line = st.tag(sent.split())
 		pos_tagged_words.append(pos_tagged_line)
 	return pos_tagged_words
 
-
-def getEntities(ner_tagged, pos_tagged):
-	entities = []
-	for i in range(0,len(ner_tagged)):
-		if ner_tagged[i][1] == 'O':
-			i+=1
-		else:
-			
 	 
 def getArticle(fileName):
 	line = fileName.readline()
 	if line != '':
 		data = json.loads(line)
-		article = data[0] 
-		return Article(article["title"], article["url"], article["text"], article["desciption"], article["articleId"])
+		article = data[0]
+		 
+		return Article(preprocessData(article["title"]), article["url"], preprocessData(article["text"]), preprocessData(article["desciption"]), article["articleId"])
 	else:
 		return ''
+	
+def preprocessData(text):
+	addSpaceTo = [":", ",", ";", "\'"]
+	temp = ""
+	for ch in addSpaceTo:
+		text = text.replace(ch, ' '+ch)
+	return text
 	
 def main():
 	'''
@@ -79,6 +110,8 @@ def main():
 		print (class_article)
 		pos_tagged = PosTagger(sent_list)
 		print (pos_tagged)
-	
+		ne_tree = getMultiWordEntityTree(class_article)
+		print(ne_tree)
+
 main()	
 #getArticle("data/test.txt")
