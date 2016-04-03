a781ddea13d0b1a31d327141c4cf60334cc206f1
preritjain93@gmail.com <preritjain93@gmail.com>
2016-04-03 21:51:35 +0530

    what

---
+++ EventExtractor/event.py
@@ -0,0 +1,12 @@
+class Event:
+  
+
+    def __init__(self, who, what, where, when, how):
+        self.who = who
+        self.what = what
+        self.where = where
+        self.when = when
+        self.how = how
+
+        
+        
